f2450e9a37d25902dff4654f6c81a2fc1ad7fac9
Vinit Patil <patilvinit83@gmail.com>
2023-05-19 02:53:48 +0530

    changed toast position

--- components/ClientProvider.tsx
+++ components/ClientProvider.tsx
@@ -5,7 +5,7 @@ import { Toaster } from 'react-hot-toast'
 const ClientProvider = () => {
     return (
         <>
-            <Toaster position="top-right" />
+            <Toaster position="top-center" />
         </>
     )
 }
